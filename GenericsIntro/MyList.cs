88a15f6e81b6231af4732017120ecf3b4fe6bfc0
Fırat <gultekinfirat651@gmail.com>
2021-02-18 13:47:41 +0300

    20 Ocak ders sonu

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //Constructor= Bir klass yenilendiğinde(New) çalışan bloğa denir.
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T item)
+        {
+            T[] tempArray = items;// Yenilenme(New) esnasında referans kodunun değişmemesi adona oluşturulan bir geçici diziyi ifade eder.Bu sayede eleman kaybı önlenmiş olur.
+            items = new T[items.Length+1];// legnth eleman sayısını ifade eder. ve yanda yazılan kod sayesinde eleman sayısı bir arttırıldı.(Dinamik olarak)
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+
+           }
+
+            items[items.Length - 1] = item;
+
+        }
+
+        public int Length
+        {
+            get { return items.Length; }
+        }
+
+        public T[] items
+        {
+            get { return items; }  
+        }
+
+
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Engin");
+
+            Console.WriteLine(isimler.Length);
+            isimler.Add("Kerem");
+
+            foreach (var isim in isimler.items)
+            {
+                Console.WriteLine(isim);
+
+            }
+
+             
+        }
+    }
+}
